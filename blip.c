cf0efde55ac79a3a5ab6c0aa3744b9e0fa256863
Dealga McArdle <tetha.z@gmail.com>
2013-05-23 12:08:27 +0300

    Create blip.c

---
+++ blip.c
@@ -0,0 +1,8 @@
+{Blip.ar(300, 17, LFNoise0.kr(0.8, 0.5, 0.5))}.play
+
+/*
+note to self, use http://en.wikipedia.org/wiki/Hodgkin%E2%80%93Huxley_model
+to simulate the behaviour of an action potential once a threshold is passed.
+
+
+*/ 
