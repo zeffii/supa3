f853d7287bd513de12f56b4171f73a2b04f3cac4
Dealga McArdle <tetha.z@gmail.com>
2013-05-23 12:42:19 +0300

    Create Line_kr.c

---
+++ Line_kr.c
@@ -0,0 +1,22 @@
+/*
+this will generate a sound that takes 0.7 seconds to go from mul 0.7 to 0.0
+*/
+{SinOsc.ar(freq: 400, mul: Line.kr(0.7, 0.0, 0.7))}.play
+
+/*
+this will generate a sound that takes 0.7 seconds to go from mul 0.7 to 0.0
+and takes 0.2 second to drop from the highest to the lower frequency,
+in essence this is a bassdrum. mono so only on one side.
+*/
+{SinOsc.ar(freq: Line.kr(600, 50, 0.03), mul: Line.kr(0.7, 0.0, 0.6))}.play
+
+/*this is a little easier to manipulate*/
+(var start_freq = 300,
+    end_freq = 50,
+    start_array,
+    end_array,
+    duration = 1.6;
+start_array = [start_freq, start_freq];
+end_array = [end_freq, end_freq];
+{SinOsc.ar(freq: Line.kr(start_array, end_array, 0.05), mul: Line.kr(0.7, 0.0, duration))}.play)
+
